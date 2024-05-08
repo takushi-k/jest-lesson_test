d9e8012a48000d084818db5f116b19c0c568b1f5
takushi <takutakushii@gmail.com>
2024-05-09 00:09:40 +0900

    u6

--- src/section3/mock_module.test.ts
+++ src/section3/mock_module.test.ts
@@ -1,13 +1,16 @@
-import fsHH from "fs";
+import fs from "fs";
 import { readFile } from "./mock_module";
 
 jest.mock("fs"); //モジュールのパスを引数に取り、そのモジュール全体を自動的にモック化することができる
-const mockFs = jest.mocked(fsHH)
+const mockFs = jest.mocked(fs)
 mockFs.readFileSync.mockReturnValue("dummy data")
 
+// jest.mock("fs", () => ({
+//   readFileSync: jest.fn().mockReturnValue("dummy data")
+// }));
 
 it("readFileがデータを返却する", () => {
   const result = readFile("path/dumy");
   expect(result).toBe("dummy data");
-  expect(fsHH.readFileSync).toHaveBeenCalledTimes(1);
+  expect(fs.readFileSync).toHaveBeenCalledTimes(1);
 })
--- src/section3/mock_module.ts
+++ src/section3/mock_module.ts
@@ -1,8 +1,10 @@
 import fs from "fs";
 
 export function readFile(path: string) {
+  console.log(path)
   const data = fs.readFileSync(path, {
     encoding: "utf-8",
   });
+  console.log(data)
   return data;
 }
